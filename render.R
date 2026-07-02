46d38ea9ee9cca113e02a965588e0bdeb23277d0
hasand <dimagmehanic@gmail.com>
2026-07-02 15:07:39 -0400

    book re-render

--- render.R
+++ render.R
@@ -1,19 +1,15 @@
 cwd <- getwd()
 
 tryCatch({
-
   setwd(file.path(cwd, "HandBook"))
-  
+
   bookdown::render_book(
     input = "index.Rmd",
     output_format = "bookdown::gitbook"
   )
-
 }, error = function(e) {
   message("Book rendering failed: ", e$message)
-  
 }, finally = {
   setwd(cwd)
   browseURL("HandBook/docs/index.html")
 })
-
